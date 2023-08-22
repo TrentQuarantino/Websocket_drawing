4cc9ca56acc6d9e1e9f4ab48186eab170994fece
Mauro <yanez@Mauros-Mac-mini.homenet.telecomitalia.it>
2023-08-22 11:31:55 +0200

    some changes on webss

--- wbsserver.py
+++ wbsserver.py
@@ -1,4 +1,4 @@
-#https://www.piesocket.com/blog/python-websocket
+
 import asyncio
 import websockets
 import json
@@ -21,24 +21,11 @@ async def handler(websocket):
     data = await websocket.recv()
     print( data)
 
-    #reply = f"Data received as: {data}!"
-    #reply = data
     websockets.broadcast(CONNECTIONS, data)
-    #initialize = await websocket.recv()
-    #initio = f"Riceit: {initialize}"
-    #await websocket.send(initio)
   
 async def main():
   start_server = websockets.serve(handler, "localhost", 8000)
   async with start_server:
     await asyncio.Future()
-#asyncio.get_event_loop().run_until_complete(start_server)
-#asyncio.get_event_loop().run_forever()
-asyncio.run(main())
 
-#https://github.com/Pithikos/python-websocket-server/blob/
-# master/websocket_server/websocket_server.py
-#https://www.tutorialspoint.com/html5/html5_websocket.htm
-#https://stackoverflow.com/questions/62205620/asyncio-how-can-i-stop-and-
-# restart-server-without-stopping-event-loop
-#https://blog.teclado.com/changes-to-async-event-loops-in-python-3-10/
+asyncio.run(main())
